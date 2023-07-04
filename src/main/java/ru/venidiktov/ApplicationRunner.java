f18dd7062cd8994b749a63b87ee36c150068a7de
GennadyVenidiktov <wenter3@outlook.com>
2023-07-04 18:02:52 +0500

    Инжектим параметры в бины из файла properties, Init и Destroy методы

--- src/main/java/ru/venidiktov/ApplicationRunner.java
+++ src/main/java/ru/venidiktov/ApplicationRunner.java
@@ -8,9 +8,10 @@ public class ApplicationRunner
 {
     public static void main( String[] args )
     {
-        var context = new ClassPathXmlApplicationContext("application.xml");
-        var pool = context.getBean("pool1", ConnectionPool.class);
-        var userRepository = context.getBean("userRepository", UserRepository.class);
-        System.out.println("OK");
+        try(var context = new ClassPathXmlApplicationContext("application.xml")) {
+            var pool = context.getBean("pool1", ConnectionPool.class);
+            var userRepository = context.getBean("userRepository", UserRepository.class);
+            System.out.println("OK");
+        }
     }
 }
--- src/main/java/ru/venidiktov/database/ConnectionPool.java
+++ src/main/java/ru/venidiktov/database/ConnectionPool.java
@@ -2,14 +2,16 @@ package ru.venidiktov.database;
 
 import java.util.List;
 import java.util.Map;
+import org.springframework.beans.factory.InitializingBean;
 
-public class ConnectionPool {
+public class ConnectionPool implements InitializingBean {
     private final String username;
     private final Integer poolSize;
     private final List<Object> args;
     private Map<String, Object> properties;
 
     public ConnectionPool(String username, Integer poolSize, List<Object> args, Map<String, Object> properties) {
+        System.out.println("Call constructor class");
         this.username = username;
         this.poolSize = poolSize;
         this.args = args;
@@ -17,6 +19,23 @@ public class ConnectionPool {
     }
 
     public void setProperties(Map<String, Object> properties) {
+        System.out.println("Call set DI");
         this.properties = properties;
+    }
+
+    private void init() {
+        System.out.print("Initialization callback. ");
+        System.out.println("Call init-method from xml");
+    }
+
+    private void destroy(){
+        System.out.print("Destroy callback. ");
+        System.out.println("Call destroy-method from xml");
+    }
+
+    @Override
+    public void afterPropertiesSet() throws Exception {
+        System.out.print("Initialization callback. ");
+        System.out.println("Call init-method from interface InitializingBean");
     }
 }
