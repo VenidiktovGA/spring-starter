75fae94e49add97a22f0eaba6de4cc1167522e92
GennadyVenidiktov <wenter3@outlook.com>
2023-07-29 19:23:38 +0500

    @Query, написание запросов через аннотацию @Query

--- src/main/java/ru/venidiktov/repo/CompanyRepository.java
+++ src/main/java/ru/venidiktov/repo/CompanyRepository.java
@@ -3,10 +3,14 @@ package ru.venidiktov.repo;
 import java.util.List;
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import ru.venidiktov.entity.Company;
 
 public interface CompanyRepository extends JpaRepository<Company, Integer> {
+    @Query("select c form Company c" +
+            "join fetch c.locales cl " +
+            " where c.name = :name")
     Optional<Company> findByName(@Param("name") String name);
     List<Company> findByNameContainingIgnoreCase(String fragment);
 }
--- src/main/java/ru/venidiktov/repo/UserRepository.java
+++ src/main/java/ru/venidiktov/repo/UserRepository.java
@@ -1,9 +1,14 @@
 package ru.venidiktov.repo;
 
 import jakarta.persistence.NamedQuery;
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import ru.venidiktov.entity.User;
 
 
 public interface UserRepository extends JpaRepository<User, Long> {
+
+    @Query("select u from User where u.firstName like %:firstName% and lastName like %:lastName%")
+    List<User> findAllLikeBy(String firstName, String lastName);
 }
